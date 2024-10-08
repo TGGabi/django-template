3ccf9c7cf3453b0d97b68cc8d87716668b36edbe
Gabriel Colmenares R. <gcolmenaresr2001@gmail.com>
2024-10-08 17:38:35 -0600

    Model sample

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey(User, verbose_name="Author", on_delete=models.CASCADE)
+    title = models.CharField(verbose_name="Title", max_length=50)
+    content = models.TextField(verbose_name="Content")
+    link = models.TextField(verbose_name="Link", max_length=300)
+    imagen = models.FileField(null=True, blank=True, upload_to='images')
+    video = models.FileField(null=True, blank=True, upload_to='video')
+    audio = models.FileField(null=True, blank=True, upload_to='audio')
+    pdf = models.FileField(null=True, blank=True, upload_to='pdf')
+    time_created = models.DateTimeField(auto_now=True)
+    time_last_edit = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
